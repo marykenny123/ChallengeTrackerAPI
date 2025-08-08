afc31f34191b1e761b0021e39880a11d8fac11b6
mary kenny <marykenny2704@yahoo.com>
2025-08-08 15:34:14 +0200

    initial commit

---
+++ src/main/java/com/femcoders/ChallengeTrackerAPI/ChallengeTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.femcoders.ChallengeTrackerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeTrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/femcoders/ChallengeTrackerAPI/ChallengeTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.femcoders.ChallengeTrackerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
