fdebc830eca223f6bfc9c694200474fd3256e066
mary kenny <marykenny2704@yahoo.com>
2025-08-27 13:53:19 +0200

    feat: add security (JwtAuthFilter, JwtService, SecurityConfig, UserDetail) and add json dependency to pom

---
+++ src/main/java/com/femcoders/ChallengeTrackerAPI/controllers/AuthController.java
@@ -0,0 +1,43 @@
+package com.femcoders.ChallengeTrackerAPI.controllers;
+
+
+import com.femcoders.ChallengeTrackerAPI.dtos.user.JwtResponse;
+import com.femcoders.ChallengeTrackerAPI.dtos.user.UserResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
+public class AuthController {
+    private final UserService userService;
+    private final AuthenticatedManager authenticationManager;
+    private final JwtService jwtService;
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtResponse> login(@RequestBody UserRequest userRequest) {
+        Authentication auth = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(userRequest.username(), userRequest.password())
+        );
+        UserDetail userDetail = (UserDetail) auth.getPrincipal();
+        String token = jwtService.generateToken(userDetail);
+        return ResponseEntity.ok(new JwtResponse(token));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> register(@Valid @RequestBody UserRequest request) {
+        UserResponse response = userService.registerUser(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+
+
+}
