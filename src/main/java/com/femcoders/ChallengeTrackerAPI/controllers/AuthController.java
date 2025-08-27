18c5f7433d23d194250cd64ca2c72e439cc77170
mary kenny <marykenny2704@yahoo.com>
2025-08-27 17:13:06 +0200

    fix:correct imports and injection and objectname in order to fix bugs (AuthController and JwtService)

--- src/main/java/com/femcoders/ChallengeTrackerAPI/controllers/AuthController.java
+++ src/main/java/com/femcoders/ChallengeTrackerAPI/controllers/AuthController.java
@@ -1,12 +1,16 @@
 package com.femcoders.ChallengeTrackerAPI.controllers;
 
-
 import com.femcoders.ChallengeTrackerAPI.dtos.user.JwtResponse;
+import com.femcoders.ChallengeTrackerAPI.dtos.user.UserRequest;
 import com.femcoders.ChallengeTrackerAPI.dtos.user.UserResponse;
+import com.femcoders.ChallengeTrackerAPI.security.UserDetail;
+import com.femcoders.ChallengeTrackerAPI.security.jwt.JwtService;
+import com.femcoders.ChallengeTrackerAPI.services.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
     private final UserService userService;
-    private final AuthenticatedManager authenticationManager;
+    private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
 
     @PostMapping("/login")
@@ -27,8 +31,8 @@ public class AuthController {
         Authentication auth = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(userRequest.username(), userRequest.password())
         );
-        UserDetail userDetail = (UserDetail) auth.getPrincipal();
-        String token = jwtService.generateToken(userDetail);
+        UserDetail userDetails = (UserDetail) auth.getPrincipal();
+        String token = jwtService.generateToken(userDetails);
         return ResponseEntity.ok(new JwtResponse(token));
     }
 
